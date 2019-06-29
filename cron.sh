e719fe0a8e2374167388f9636553795009207c9b
Volker <lists.volker@gmail.com>
2019-06-29 10:03:55 -0400

    Kill docker container if its stuck

--- cron.sh
+++ cron.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+DOCKER_NAME=lenovo-scrapper
+
+if docker ps|grep "$DOCKER_NAME" > /dev/null; then
+    if docker ps -f name="$DOCKER_NAME" --format '{{ .RunningFor }}'|grep "[1-9][0-9] min" > /dev/null; then
+        echo "process is running for a while"
+        docker ps -f name="$DOCKER_NAME" --format '{{ .RunningFor }}' || true
+        docker kill "$DOCKER_NAME"
+    elif docker ps -f name="$DOCKER_NAME" --format '{{ .RunningFor }}'|grep "hour" > /dev/null; then
+        echo "process is running for a while"
+        docker ps -f name="$DOCKER_NAME" --format '{{ .RunningFor }}' || true
+        docker kill "$DOCKER_NAME"
+    fi
+
+    # docker is running already
+    exit
+fi
+
+
+
 docker run --rm \
     --name lenovo-scrapper \
     -v "$HOME/localdata/lenovo-outlet-scrapper/example.db:/tmp/example.db" \
